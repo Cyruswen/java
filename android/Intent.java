2d732f2d3c76f8a15331914342a1563f989b8005
Cyruswen <Cyrus_wen@163.com>
2018-06-24 19:56:15 +0800

    add Intent.java

---
+++ android/Intent.java
@@ -0,0 +1,7 @@
+public class Activity1 extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        RadioGroup RG_OS;
+        RadioButton RG_OS_RB1, RG_OS_RB2, RG_OSRB3;
+        Button button_submit, button_back;
+    }
+}
